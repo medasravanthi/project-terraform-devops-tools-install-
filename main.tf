c98e97f09551a175d919c79b35a5c23db538c761
medasravanthi <150466318+medasravanthi@users.noreply.github.com>
2024-02-03 20:09:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-  ami                    = "ami-0277155c3f0ab2930"      #change ami id for different region
+  ami                    = "ami-0ecb0bb5d6b19457a"      #change ami id for different region
   instance_type          = "t2.large"
-  key_name               = "vscode"              #change key name as per your setup
+  key_name               = "jenkin"              #change key name as per your setup
   vpc_security_group_ids = [aws_security_group.devops-project-veera.id]
   user_data              = templatefile("./install.sh", {})
 
